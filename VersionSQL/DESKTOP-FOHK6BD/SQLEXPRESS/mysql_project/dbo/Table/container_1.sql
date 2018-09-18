8367c29d7226c969896cf88f1bc9705596a12931
Ankitaarvind1 <ankita.arvind3@gmail.com>
2018-09-18 23:30:10 +0530

    column typeof added

--- VersionSQL/DESKTOP-FOHK6BD/SQLEXPRESS/mysql_project/dbo/Table/container_1.sql
+++ VersionSQL/DESKTOP-FOHK6BD/SQLEXPRESS/mysql_project/dbo/Table/container_1.sql
@@ -5,6 +5,7 @@ SET QUOTED_IDENTIFIER ON
 CREATE TABLE [dbo].[container_1](
 	[name] [varchar](50) NULL,
 	[ID] [int] NULL,
-	[description] [nvarchar](50) NULL
+	[description] [nvarchar](50) NULL,
+	[typeof] [varchar](50) NULL
 ) ON [PRIMARY]
 
