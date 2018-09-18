712fbfab691b2e27342a9d999733e98e98f5f24b
Ankitaarvind1 <ankita.arvind3@gmail.com>
2018-09-18 23:20:21 +0530

    first commit of test sql

---
+++ VersionSQL/DESKTOP-FOHK6BD/SQLEXPRESS/mysql_project/dbo/Table/container_1.sql
@@ -0,0 +1,10 @@
+/****** Object:  Table [dbo].[container_1]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[container_1](
+	[name] [varchar](50) NULL,
+	[ID] [int] NULL,
+	[description] [nvarchar](50) NULL
+) ON [PRIMARY]
+
